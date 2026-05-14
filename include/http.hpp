1e5d6b2b5838af0a524fd80cdb8c33206d891f4f
Mateo LE FLEM <mle-flem@student.42.fr>
2026-05-14 20:27:06 +0200

    refactor: doing some X macro magic for enums

--- include/http.hpp
+++ include/http.hpp
@@ -6,27 +6,45 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 04:03:45 by mle-flem          #+#    #+#             */
-/*   Updated: 2026/05/13 04:05:09 by mle-flem         ###   ########.fr       */
+/*   Updated: 2026/05/14 20:17:46 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <cstddef>
+
 namespace http {
 
 namespace methods {
-enum HttpMethods {
-    GET = 0,
-    HEAD,
-    POST,
-    PUT,
-    DELETE,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-    COUNT,
+
+#define HTTP_METHODS                                                           \
+    X(GET)                                                                     \
+    X(POST)                                                                    \
+    X(DELETE)                                                                  \
+    X(PUT)                                                                     \
+    X(PATCH)                                                                   \
+    X(HEAD)                                                                    \
+    X(OPTIONS)                                                                 \
+    X(CONNECT)                                                                 \
+    X(TRACE)
+
+enum type {
+#define X(name) name,
+    HTTP_METHODS
+#undef X
 };
+
+static const char *strings[] = {
+#define X(name) #name,
+    HTTP_METHODS
+#undef X
+};
+
+static const std::size_t COUNT = sizeof(strings) / sizeof(*strings);
+
 }
 
 }
+
+#undef HTTP_METHODS
