84a17f20a3e7f43a160517debdcc63833879b331
Mateo LE FLEM <mle-flem@student.42.fr>
2026-05-27 21:20:25 +0200

    fix: add back missing query field in http::request

--- include/http.hpp
+++ include/http.hpp
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 04:03:45 by mle-flem          #+#    #+#             */
-/*   Updated: 2026/05/19 06:26:41 by mle-flem         ###   ########.fr       */
+/*   Updated: 2026/05/27 21:19:21 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,6 +139,7 @@ struct request {
     std::string uri;
     versions::type version;
     std::map<std::string, std::string> headers;
+    std::map<std::string, std::string> query;
     std::string body;
 };
 
