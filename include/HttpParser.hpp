bf14b242f3704127dd4d6cb9a71d86c4c52be99d
Mateo LE FLEM <mle-flem@student.42.fr>
2026-06-03 22:00:50 +0200

    refactor: strip the whole HttpParser for now

--- include/HttpParser.hpp
+++ include/HttpParser.hpp
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 20:54:11 by mle-flem          #+#    #+#             */
-/*   Updated: 2026/05/27 21:22:17 by mle-flem         ###   ########.fr       */
+/*   Updated: 2026/06/03 22:00:00 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,14 @@ public:
 
     bool feed(const char *data, std::size_t len);
     void set_eof();
+    void reset();
 
     bool is_complete() const { return _state == COMPLETE; }
     bool is_error() const { return _state == ERROR; }
     bool is_eof() const { return _eof; }
 
     http::status::type error_code() const { return _error_code; }
-
     const http::request &request() const { return _request; }
-
-    void reset();
 
 private:
     std::string _buf;
@@ -54,16 +52,4 @@ private:
 
     bool run();
     void set_err(http::status::type code);
-
-    bool try_parse_request_line();
-    bool try_parse_method(std::size_t start, std::size_t end);
-    bool try_parse_uri(std::size_t start, std::size_t end);
-    bool try_parse_query(std::size_t start, std::size_t end);
-    bool try_parse_version(std::size_t start, std::size_t end);
-
-    bool try_parse_headers();
-    bool try_parse_header_field(std::size_t &pos, std::size_t end_pos);
-
-    bool try_parse_body();
-    bool try_parse_chunk();
 };
