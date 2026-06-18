4ca4a552e4d5c11058d30ebbc7f12a0911311487
LAPORTENicolas <laportenicolas@protonmail.com>
2026-06-18 19:57:31 +0200

    chore: using anon namespace, rename Parser into ConfigParser

--- test/config_parsing.cpp
+++ test/config_parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: nlaporte <nlaporte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 00:10:43 by nlaporte          #+#    #+#             */
-/*   Updated: 2026/06/17 21:10:00 by nlaporte         ###   ########.fr       */
+/*   Updated: 2026/06/18 20:16:35 by nlaporte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,131 @@
 #include <unistd.h>
 
 #include <cstdint>
+#include <cstdlib>
 #include <vector>
 
 #include "config-parser.hpp"
 #include "logger.hpp"
 
 // Test parsing utils
+
+bool size_check(const std::string &val)
+{
+    std::size_t i;
+    for (i = 0; val[i]; i++) {
+        if (!std::isdigit(val[i]))
+            break;
+    }
+    if (!val[i])
+        return true;
+    if ((val[i] == 'k' || val[i] == 'K') && i + 1 == (val.size()))
+        return true;
+    if ((val[i] == 'm' || val[i] == 'M') && i + 1 == (val.size()))
+        return true;
+    if ((val[i] == 'g' || val[i] == 'G') && i + 1 == (val.length()))
+        return true;
+    return (std::isdigit(val[i]));
+}
+
+std::size_t convert_string_to_size(const std::string &val)
+{
+    int64_t i;
+    char *p;
+
+    i = std::strtol(val.c_str(), &p, 10);
+    if (!*p)
+        return i;
+    if (*p == 'k' || *p == 'K')
+        return (INT64_MAX / 1024 < i ? INT64_MAX : i * 1024);
+    if (*p == 'm' || *p == 'M')
+        return (INT64_MAX / 1048576 < i ? INT64_MAX : i * 1048576);
+    if ((*p == 'g' || *p == 'G'))
+        return (INT64_MAX / 1073741824 < i ? INT64_MAX : i * 1073741824);
+    return 0;
+}
+
+std::size_t convert_time_to_size_t(const std::string &val)
+{
+    int64_t i = 0;
+    std::size_t final_size = 0;
+    const char *str = val.c_str();
+    char *p;
+
+    while (1) {
+        i = std::strtol(str, &p, 10);
+        if (i < 0)
+            return 9;
+        str = p;
+        if (!*str)
+            return final_size + (i / 1000);
+        if (str[1] && str[0] == 'm' && str[1] == 's') {
+            final_size += (i / 1000);
+            str += 2;
+        } else {
+            switch (*str) {
+            case 's':
+                final_size += i;
+                str++;
+                break;
+            case 'm':
+                final_size += i * 60;
+                str++;
+                break;
+            case 'h':
+                final_size += i * 3600;
+                str++;
+                break;
+                /*
+        case 'd':
+        case 'w':
+        case 'M':
+        case 'y':
+                */
+            default:
+                return final_size;
+            }
+        }
+        if (!*p)
+            return final_size;
+    }
+    return 0;
+}
+
+bool time_check(const std::string &val)
+{
+    bool valid = false;
+    uint32_t i = 0;
+
+    while (1) {
+        while (val[i] && std::isdigit(val[i])) {
+            i++;
+        }
+        if (i + 1 < val.length() && val[i] == 'm' && (val[i + 1]) == 's') {
+            valid = true;
+            i += 2;
+        } else {
+            switch (val[i]) {
+            case 's':
+            case 'm':
+            case 'h':
+                /*
+            case 'd':
+            case 'w':
+            case 'M':
+            case 'y':
+*/
+                i++;
+                valid = true;
+                break;
+            default:
+                return (!val[i]);
+            }
+        }
+        if (i == val.length())
+            return valid;
+    }
+    return valid;
+}
 
 Test(config_parsing, time_utils)
 {
@@ -118,16 +237,16 @@ Test(config_parsing, good_simple)
     std::vector<Server> servers;
 
     std::string path = "test/data/conf-0.conf";
-    cr_assert_eq(Parser::parse_config(path, servers), true);
+    cr_assert_eq(ConfigParser::parse_config(path, servers), true);
     cr_assert_eq(servers.size(), 1);
     servers.clear();
 
     path = "test/data/conf-1.conf";
-    cr_assert_eq(Parser::parse_config(path, servers), true);
+    cr_assert_eq(ConfigParser::parse_config(path, servers), true);
     cr_assert_eq(servers.size(), 2);
     servers.clear();
 
     path = "test/data/conf-3.conf";
-    cr_assert_eq(Parser::parse_config(path, servers), false);
+    cr_assert_eq(ConfigParser::parse_config(path, servers), false);
     servers.clear();
 }
