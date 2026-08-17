8743354e44baff082b3b6d10d8566fbc02bae976
Mateo LE FLEM <mle-flem@student.42.fr>
2026-08-17 17:14:55 +0200

    feat: add a global try catch in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 18:53:25 by mle-flem          #+#    #+#             */
-/*   Updated: 2026/07/17 19:07:46 by mle-flem         ###   ########.fr       */
+/*   Updated: 2026/08/14 05:08:15 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,27 @@
 
 int32_t main(int32_t ac, char **av)
 {
-    logger::print_date() = false;
-    std::vector<Server> servers;
+    try {
+        logger::print_date() = false;
+        std::vector<Server> servers;
 
-    cli::ParsedArgs args = cli::parse_arguments(ac, av);
-    if (args.should_quit)
+        cli::ParsedArgs args = cli::parse_arguments(ac, av);
+        if (args.should_quit)
+            return 1;
+
+        if (args.flags[cli::flags::SILENT])
+            logger::log_level() = logger::levels::NOTHING;
+        if (args.flags[cli::flags::VERBOSE])
+            logger::log_level() = logger::levels::TRACE;
+
+        if (!ConfigParser::parse_config(args.config_path, servers))
+            return 1;
+
+        logger::print_date() = true;
+        EventLoop loop(servers);
+        return loop.run() ? 0 : 1;
+    } catch (const std::exception &e) {
+        std::cerr << "An exception occurred: " << e.what() << '\n';
         return 1;
-
-    if (args.flags[cli::flags::SILENT])
-        logger::log_level() = logger::levels::NOTHING;
-    if (args.flags[cli::flags::VERBOSE])
-        logger::log_level() = logger::levels::TRACE;
-
-    if (!ConfigParser::parse_config(args.config_path, servers))
-        return 1;
-
-    logger::print_date() = true;
-    EventLoop loop(servers);
-    return loop.run() ? 0 : 1;
+    }
 }
